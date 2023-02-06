14e392382e7fab3ee14a23494651053422dd1362
Priya <sharma27598@gmail.com>
2023-02-06 11:44:04 +0530

    Calculator project

--- src/Calculator.java
+++ src/Calculator.java
@@ -156,6 +156,13 @@ public class Calculator implements ActionListener {
         if(e.getSource() == clrButton){
             textfield.setText("");
         }
+        if(e.getSource() == delButton){
+            String string = textfield.getText();
+            textfield.setText("");
+            for(int i = 0; i < string.length()-1; i++){
+                textfield.setText(textfield.getText()+string.charAt(i));
+            }
+        }
 
     }
 }
